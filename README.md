27ba618255f17755b82d3e02dbb7471c17f6e6c1
Mahmut Ozge Karakaya <mokarakaya@gmail.com>
2020-10-03 12:55:16 +0200

    edit urls

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # azure-flask
+
+- Deploys a Flask Web application to Azure by using Github Actions
+- App url: https://mokarakaya-hello-flask.azurewebsites.net/hello
--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/hello")
+def hello():
+    return "<h1>Hello Azure! hello</h1>"
+
+@app.route("/")
 def index():
-    return "<h1>Hello Azure! updated</h1>"
+    return "<h1>Hello Azure! index page</h1>"
